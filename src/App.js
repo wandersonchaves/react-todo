9bdfc5b0be4ddda6552e2cfd2753fa78c40e2b90
Wanderson Chaves <wandersonscpibr@gmail.com>
2020-12-29 21:34:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// Importing Components
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Ed's Todo List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
